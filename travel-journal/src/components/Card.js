bc87c576ab1addf4d809fd10f9dcff4d1829cacc
l-erw <lizzieerwood@gmail.com>
2022-01-11 17:14:27 +0000

    template for card component

--- travel-journal/src/components/Card.js
+++ travel-journal/src/components/Card.js
@@ -1,12 +1,13 @@
 import React from "react"
 import MapMarker from "../map-marker.png"
+import MountFuji from "../mount-fuji.jpg"
 
 export default function Card() {
     return (
         <div className="card">
         <img src={MapMarker} alt="map marker dot"/>
         <div className="card--data">
-            <img src="" alt=""/>
+            <img className="card--image" src={MountFuji} alt="Mount Fuji"/>
             <p>Location</p>
             <a href="maps.google.com">View on Google Maps</a>
         </div>
