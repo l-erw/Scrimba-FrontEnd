178d1b9eb71a31d0696a598318f2e0e60521c981
l-erw <lizzieerwood@gmail.com>
2022-01-11 16:13:57 +0000

    icons added

--- travel-journal/src/components/Card.js
+++ travel-journal/src/components/Card.js
@@ -1,7 +1,19 @@
 import React from "react"
+import MapMarker from "../map-marker.png"
 
 export default function Card() {
-    <div>
-        <h1>123</h1>
+    <div className="card">
+        <img src={MapMarker} alt="map marker dot"/>
+        <div className="card--data">
+            <img src="" alt=""/>
+            <p>Location</p>
+            <a href="maps.google.com">View on Google Maps</a>
+        </div>
+        <div className="card--content">
+            <h2>Card Title</h2>
+            <p>Card dates</p>
+            <p>Card description</p>
+
+        </div>
     </div>
 }
--- travel-journal/src/components/Navbar.js
+++ travel-journal/src/components/Navbar.js
@@ -1,8 +1,9 @@
 import React from "react"
+import Globe from "../globe.png"
 
 export default function Navbar() {
     <nav>
-        <img src="" alt="globe"/>
+        <img src={Globe} alt="globe"/>
         <h1 className="nav--header">my travel journal.</h1>
     </nav>
 }
