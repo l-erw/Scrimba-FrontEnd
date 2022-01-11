ba9a77f0a9d3338cd3ee9cdd7039cf342b973d82
l-erw <lizzieerwood@gmail.com>
2022-01-11 15:53:16 +0000

    img and h1 for navbar

--- travel-journal/src/components/Navbar.js
+++ travel-journal/src/components/Navbar.js
@@ -3,6 +3,6 @@ import React from "react"
 export default function Navbar() {
     <nav>
         <img src="" alt="globe"/>
-        <p>my travel journal</p>
+        <h1 className="nav--header">my travel journal.</h1>
     </nav>
 }
