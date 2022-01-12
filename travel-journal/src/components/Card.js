2a9d912c8f1eb6bdcd23c570f2cf69e011f79fc4
l-erw <lizzieerwood@gmail.com>
2022-01-12 08:03:40 +0000

    added horizontal line at card bottom

--- travel-journal/src/components/Card.js
+++ travel-journal/src/components/Card.js
@@ -26,6 +26,47 @@ export default function Card() {
                 </div>
             </div>
 
+            <div className="card">
+                <div>
+                <img className="card--image" src={MountFuji} alt="Mount Fuji"/>
+                </div>
+            
+                <div className="layout-test">
+                    <div className="card--data">
+                        <img src={MapMarker} alt="map marker dot"/>
+                        <p className="card--location">Japan</p>
+                        <a className="card--link" href="maps.google.com">View on Google Maps</a>
+                    </div>
+                    
+                    <div className="card--content">
+                        <h2>Mount Fuji</h2>
+                        <p className="card--dates">12 Jan, 2021 - 24 Jan, 2021</p>
+                        <p className="card--description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji 
+                            is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="card">
+                <div>
+                <img className="card--image" src={MountFuji} alt="Mount Fuji"/>
+                </div>
+            
+                <div className="layout-test">
+                    <div className="card--data">
+                        <img src={MapMarker} alt="map marker dot"/>
+                        <p className="card--location">Japan</p>
+                        <a className="card--link" href="maps.google.com">View on Google Maps</a>
+                    </div>
+                    
+                    <div className="card--content">
+                        <h2>Mount Fuji</h2>
+                        <p className="card--dates">12 Jan, 2021 - 24 Jan, 2021</p>
+                        <p className="card--description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji 
+                            is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                    </div>
+                </div>
+            </div>
         </section>
 
     )
