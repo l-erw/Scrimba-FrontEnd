6b012532a54571eeb3627e19722f0f65da7121e2
l-erw <lizzieerwood@gmail.com>
2022-01-12 07:23:54 +0000

    card styles

--- travel-journal/src/components/Card.js
+++ travel-journal/src/components/Card.js
@@ -4,20 +4,29 @@ import MountFuji from "../mount-fuji.jpg"
 
 export default function Card() {
     return (
-        <div className="card">
-        <img src={MapMarker} alt="map marker dot"/>
-        <div className="card--data">
-            <img className="card--image" src={MountFuji} alt="Mount Fuji"/>
-            <p>Location</p>
-            <a href="maps.google.com">View on Google Maps</a>
-        </div>
-        <div className="card--content">
-            <h2>Card Title</h2>
-            <p>Card dates</p>
-            <p>Card description</p>
+        <section>
+            <div className="card">
+                <div>
+                <img className="card--image" src={MountFuji} alt="Mount Fuji"/>
+                </div>
+            
+                <div className="layout-test">
+                    <div className="card--data">
+                        <img src={MapMarker} alt="map marker dot"/>
+                        <p>Location</p>
+                        <a href="maps.google.com">View on Google Maps</a>
+                    </div>
+                    
+                    <div className="card--content">
+                        <h2>Card Title</h2>
+                        <p>Card dates</p>
+                        <p>Card description</p>
+                    </div>
+                </div>
+            </div>
 
-        </div>
-    </div>
+        </section>
+
     )
 
 }
