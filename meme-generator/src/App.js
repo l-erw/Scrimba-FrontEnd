fc983922f31c8a132e16319ef7d5ce3003afb50b
l-erw <lizzieerwood@gmail.com>
2022-01-18 11:05:34 +0000

    page layout setup

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Form from './components/Form';
 
+
 function App() {
   return (
     <div className="App">
--- meme-generator/src/components/Form.js
+++ meme-generator/src/components/Form.js
@@ -1,12 +1,19 @@
 import React from "react"
 
 export default function Form() {
+
+    function getMemeImage() {
+        console.log("random meme")
+    }
     return (
-        <form>
-            <input/>
-            <input/>
-            <button className="form--btn">Get a new meme image 🖼️</button>
+        <main>
+            <form>
+                <input type="text" id="meme-input-top" aria-label="meme input top" placeholder="text here"/>
+                <input type="text" id="meme-input-bottom" aria-label="meme input bottom" placeholder="text here"/>
+                <button className="form--btn" onClick={getMemeImage}>Get a new meme image 🖼️</button>
+            </form>
             <div className="meme--placeholder"></div>
-        </form>
+        </main>
+        
     )
 }
